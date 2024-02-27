6ee005577b97a51a3e703a0da1071e06792d1059
aconstlink <aconstlink@gmail.com>
2024-02-27 11:57:38 +0100

    more on tests.

--- suite_graphics/10_03_gs_streamout.cpp
+++ suite_graphics/10_03_gs_streamout.cpp
@@ -2,6 +2,8 @@
 #include <motor/platform/global.h>
 #include <motor/application/window/window_message_listener.h>
 
+#include <motor/tool/imgui/custom_widgets.h>
+
 #include <motor/graphics/object/render_object.h>
 #include <motor/graphics/object/geometry_object.h>
 #include <motor/graphics/object/msl_object.h>
@@ -23,8 +25,12 @@ namespace this_file
         bool_t graphics_init = false ;
         motor::vector< bool_t > rnd_init ;
 
-        motor::graphics::geometry_object_t geo_obj ;
+        motor::graphics::streamout_object_t so_obj ;
+        motor::graphics::geometry_object_t geo_pts_obj ;
+        motor::graphics::msl_object_t msl_filter_obj ;
         motor::graphics::msl_object_t msl_obj ;
+
+        motor::math::vec3f_t dir = motor::math::vec3f_t( -1.0f, 0.0f, 0.0f ) ;
 
         virtual void_t on_init( void_t ) noexcept
         {
@@ -75,70 +81,176 @@ namespace this_file
             }
         }
 
-        virtual void_t on_update( motor::application::app::update_data_in_t ) noexcept {}
-
         virtual void_t on_graphics( motor::application::app::graphics_data_in_t gd ) noexcept 
         {
             if( !graphics_init ) 
             {
-                // geometry configuration for post quad
+                // geometry configuration : points
                 {
+                    size_t const w = 100 ;
+                    size_t const h = 100 ;
+
+                    float_t const sx = 1.0f/w ; //1.0f ;// float_t( w ) ;
+                    float_t const sy = 1.0f/h ; // 1.0f ;// float_t( h ) ;
+
+                    motor::math::vec2f_t const start = motor::math::vec2f_t( -float_t( 1.0f ), -float_t( 1.0f ) ) *0.5f;
+
                     struct vertex { motor::math::vec4f_t pos ; motor::math::vec4f_t color ; } ;
 
                     auto vb = motor::graphics::vertex_buffer_t()
                         .add_layout_element( motor::graphics::vertex_attribute::position, motor::graphics::type::tfloat, motor::graphics::type_struct::vec4 )
                         .add_layout_element( motor::graphics::vertex_attribute::color0, motor::graphics::type::tfloat, motor::graphics::type_struct::vec4 )
-                        .resize( 4 ).update<vertex>( [=] ( vertex* array, size_t const ne )
+                        .resize( w * h ).update<vertex>( [=] ( vertex* array, size_t const ne )
                     {
-                        array[ 0 ].pos = motor::math::vec4f_t( -0.5f, -0.5f, 0.0f, 1.0f ) ;
-                        array[ 1 ].pos = motor::math::vec4f_t( -0.5f, +0.5f, 0.0f, 1.0f ) ;
-                        array[ 2 ].pos = motor::math::vec4f_t( +0.5f, +0.5f, 0.0f, 1.0f ) ;
-                        array[ 3 ].pos = motor::math::vec4f_t( +0.5f, -0.5f, 0.0f, 1.0f ) ;
+                        for( size_t yi =0; yi<h; ++yi )
+                        {
+                            for( size_t xi =0; xi<w; ++xi )
+                            {
+                                size_t const idx = yi * w + xi ;
 
-                        array[ 0 ].color = motor::math::vec4f_t( 1.0f, 1.0f, 1.0f, 1.0f ) ;
-                        array[ 1 ].color = motor::math::vec4f_t( 1.0f, 1.0f, 1.0f, 1.0f ) ;
-                        array[ 2 ].color = motor::math::vec4f_t( 1.0f, 1.0f, 1.0f, 1.0f ) ;
-                        array[ 3 ].color = motor::math::vec4f_t( 1.0f, 1.0f, 1.0f, 1.0f ) ;
+                                motor::math::vec2f_t const p = start + motor::math::vec2f_t( sx, sy ) * 
+                                    motor::math::vec2f_t( float_t(xi), float_t(yi) ) ;
+
+                                array[ idx ].pos = motor::math::vec4f_t( p, 0.0f, sx*sy*0.5f ) ;
+                                array[ idx ].color = motor::math::vec4f_t( -1.0f, 1.0f, 1.0f, 1.0f ) ;
+                            }
+                        }
+                    
                     } );
 
-                    auto ib = motor::graphics::index_buffer_t().
-                        set_layout_element( motor::graphics::type::tuint ).resize( 6 ).
-                        update<uint_t>( [] ( uint_t* array, size_t const ne )
+                    geo_pts_obj = motor::graphics::geometry_object_t( "points",
+                        motor::graphics::primitive_type::points, std::move( vb ) ) ;
+
+                    // stream out object configuration
                     {
-                        array[ 0 ] = 0 ;
-                        array[ 1 ] = 1 ;
-                        array[ 2 ] = 2 ;
+                        auto vb2 = motor::graphics::vertex_buffer_t()
+                            .add_layout_element( motor::graphics::vertex_attribute::position, motor::graphics::type::tfloat, motor::graphics::type_struct::vec4 )
+                            .add_layout_element( motor::graphics::vertex_attribute::color0, motor::graphics::type::tfloat, motor::graphics::type_struct::vec4 ) ;
 
-                        array[ 3 ] = 0 ;
-                        array[ 4 ] = 2 ;
-                        array[ 5 ] = 3 ;
-                    } ) ;
-
-                    geo_obj = motor::graphics::geometry_object_t( "points",
-                        motor::graphics::primitive_type::points, std::move( vb ), std::move( ib ) ) ;
+                        so_obj = motor::graphics::streamout_object_t( "filtered_points", std::move( vb2 ) ).resize( w * h ) ;
+                    }
                 }
 
-                // render texture array
+                // filter points shader
                 {
-                    motor::graphics::msl_object_t mslo("scene_obj") ;
+                    motor::graphics::msl_object_t mslo("filter_points") ;
+
+                    mslo.add( motor::graphics::msl_api_type::msl_4_0, R"(
+                    config filter_stream
+                    {
+                        vertex_shader
+                        {
+                            inout vec4_t pos : position ;
+                            inout vec4_t color : color ;
+                            
+                            uint_t vid : vertex_id ;
+
+                            void main()
+                            {
+                                out.pos = in.pos ; 
+                                out.color = in.color ; 
+                            }
+                        }
+
+                        geometry_shader
+                        {
+                            in points ;
+                            out points[ max_verts = 1 ] ;
+
+                            inout vec4_t pos : position ;
+                            inout vec4_t color : color ;
+
+                            uint_t pid : primitive_id ;
+
+                            vec3_t plane ;
+
+                            void main()
+                            {
+                                for( int i=0; i<in.length(); i++ )
+                                {
+                                    out.pos = in[i].pos ;
+                                    out.color = in[i].color ;
+                                    
+                                    // recolor every 2nd vertex
+                                    //if( pid % 2 == 0 ) { out.color = vec4_t( 0.0, 1.0, 1.0, 1.0 ) ; }
+
+                                    float_t weight = in[i].pos.w ;
+                                    float_t pdp = dot( plane, in[i].pos.xyz ) ;
+                        
+                                    // on the line
+                                    if( (pdp > -0.001) && (pdp < 0.001) ) 
+                                    { 
+                                        out.pos.w = in[i].pos.w ;//0.005 ;
+                                        out.color = vec4_t( 0.0, 0.0, 1.0, 1.0 ) ' in[i].color ;
+                                        emit_vertex() ; 
+                                    }
+
+                                    // "under the line"
+                                    else if( pdp < 0.0 ) 
+                                    { 
+                                        out.pos.w = in[i].pos.w;//0.0025 ;
+                                        out.color = vec4_t( 1.0, 0.0, 0.0, 1.0 ) ' in[i].color ;
+                                        emit_vertex() ; 
+                                    }
+
+                                    // "above" the line
+                                    else if( pdp > 0.0 ) 
+                                    { 
+                                        out.pos.w = in[i].pos.w ;//0.005 ;
+                                        out.color = vec4_t( 0.0, 1.0, 0.0, 1.0 ) ' in[i].color ;
+                                        //emit_vertex() ; 
+                                    }
+                                }
+                                end_primitive() ;
+                            }
+                        }
+                    })" ) ;
+
+                        
+                    mslo.link_geometry( "points" ) ;
+
+                    // add variable set
+                    {
+                        motor::graphics::variable_set_t vars ;
+                        {}
+                        mslo.add_variable_set( motor::shared( std::move( vars ) ) ) ;
+                    }
+
+                    msl_filter_obj = std::move( mslo ) ;
+                }
+
+                // render shader
+                {
+                    motor::graphics::msl_object_t mslo("render") ;
 
                     mslo.add( motor::graphics::msl_api_type::msl_4_0, R"(
                     config render_original
                     {
                         vertex_shader
                         {
-                            inout vec4_t pos : position ;
-                            inout vec4_t color : color ;
+                            in vec4_t pos : position ;
+                            in vec4_t color : color ;
 
-                            mat4_t u_proj : projection ;
-                            mat4_t u_view : view ;
-                            mat4_t u_world : world ;
+                            out vec4_t pos : position ;
+                            out vec4_t color : color ;
+        
+                            uint_t vid : vertex_id ;
                             
+                            data_buffer_t u_filtered ;
+
+                            out float_t scale ;
+
                             void main()
                             {
-                                out.pos = vec4_t( in.pos.xyz, 1.0 ) ;
-                                out.pos = u_proj * u_view * u_world * out.pos ;
-                                out.color = in.color ;
+                                int_t idx = vid ;
+                                vec4_t pos = fetch_data( u_filtered, (idx << 1) + 0 ) ; 
+                                vec4_t color = fetch_data( u_filtered, (idx << 1) + 1 ) ;
+
+                                out.scale = 0.0025;//pos.w * 2.0 ;
+
+                                out.pos = vec4_t( pos.xyz, 1.0 ) ;
+                                //out.pos = out.pos ;
+                                out.color = color ' in.color;
                             }
                         }
 
@@ -150,39 +262,41 @@ namespace this_file
                             inout vec4_t pos : position ;
                             inout vec4_t color : color ;
 
+                            in float_t scale ;
+
                             void main()
                             {
                                 for( int i=0; i<in.length(); i++ )
                                 {
                                     {
-                                        out.pos = vec4_t( in[i].pos.xy + vec2_t( -0.25, -0.25 ), 0.0, in[i].pos.w ) ;
+                                        out.pos = vec4_t( in[i].pos.xy + vec2_t( -in[i].scale, -in[i].scale ), 0.0, 1.0 ) ;
                                         out.color = in[i].color ;
                                         emit_vertex() ;
                                     }
                                     {
-                                        out.pos =  vec4_t( in[i].pos.xy + vec2_t( -0.25, 0.25 ), 0.0, in[i].pos.w ) ;
+                                        out.pos =  vec4_t( in[i].pos.xy + vec2_t( -in[i].scale, in[i].scale ), 0.0, 1.0 ) ;
                                         out.color = in[i].color ;
                                         emit_vertex() ;
                                     }
                                     {
-                                        out.pos =  vec4_t( in[i].pos.xy + vec2_t( +0.25, +0.25 ), 0.0, in[i].pos.w ) ;
+                                        out.pos =  vec4_t( in[i].pos.xy + vec2_t( +in[i].scale, +in[i].scale ), 0.0, 1.0 ) ;
                                         out.color = in[i].color ;
                                         emit_vertex() ;
                                     }
                                     end_primitive() ;
 
                                     {
-                                        out.pos =  vec4_t( in[i].pos.xy + vec2_t( -0.25, -0.25 ), 0.0, in[i].pos.w ) ;
+                                        out.pos =  vec4_t( in[i].pos.xy + vec2_t( -in[i].scale, -in[i].scale ), 0.0, 1.0 ) ;
                                         out.color = in[i].color ;
                                         emit_vertex() ;
                                     }
                                     {
-                                        out.pos =  vec4_t( in[i].pos.xy + vec2_t( 0.25, 0.25 ), 0.0, in[i].pos.w ) ;
+                                        out.pos =  vec4_t( in[i].pos.xy + vec2_t( in[i].scale, in[i].scale ), 0.0, 1.0 ) ;
                                         out.color = in[i].color ;
                                         emit_vertex() ;
                                     }
                                     {
-                                        out.pos =  vec4_t( in[i].pos.xy + vec2_t( 0.25, -0.25 ), 0.0, in[i].pos.w ) ;
+                                        out.pos =  vec4_t( in[i].pos.xy + vec2_t( in[i].scale, -in[i].scale ), 0.0, 1.0 ) ;
                                         out.color = in[i].color ;
                                         emit_vertex() ;
                                     }
@@ -202,13 +316,17 @@ namespace this_file
                         }
                     })" ) ;
 
-                        
-                    mslo.link_geometry( "points" ) ;
+                    // must set soo name so number of captured primitives can be 
+                    // retrieved from the streamout object.
+                    mslo.link_geometry( "points", "filtered_points" ) ;
 
                     // add variable set
                     {
                         motor::graphics::variable_set_t vars ;
-                        {}
+                        {
+                            auto* var = vars.array_variable_streamout( "u_filtered" ) ;
+                            var->set( "filtered_points" ) ;
+                        }
                         mslo.add_variable_set( motor::shared( std::move( vars ) ) ) ;
                     }
 
@@ -227,17 +345,73 @@ namespace this_file
             {
                 rnd_init[wid] = true ;
 
-                fe->configure<motor::graphics::geometry_object_t>( &geo_obj ) ;
+                fe->configure<motor::graphics::geometry_object_t>( &geo_pts_obj ) ;
+                fe->configure<motor::graphics::streamout_object_t>( &so_obj ) ;
+                fe->configure<motor::graphics::msl_object_t>( &msl_filter_obj ) ;
                 fe->configure<motor::graphics::msl_object_t>( &msl_obj ) ;
+
+                // do initial streamout pass 
+                // in order to fill the buffer
+                {
+                    fe->use( &so_obj ) ;
+                    fe->render( &msl_filter_obj, motor::graphics::gen4::backend::render_detail_t() ) ;
+                    fe->unuse( motor::graphics::gen4::backend::unuse_type::streamout ) ;
+                }
             }
 
             {
+                fe->use( &so_obj ) ;
+                motor::graphics::gen4::backend::render_detail_t detail ;
+                detail.feed_from_streamout = false ;
+                fe->render( &msl_filter_obj, detail ) ;
+                fe->unuse( motor::graphics::gen4::backend::unuse_type::streamout ) ;
+            }
+
+            // draw the cutting plane
+            {
+                auto dir2 = dir.xy().normalized().ortho() ;
+                motor::math::mat2f_t basis ;
+                basis.set_column( 0, dir ) ;
+                basis.set_column( 1, dir2 ) ;
+
+                motor::math::vec2f_t p0 = basis * motor::math::vec2f_t(0.0f, -250.0f ) ;
+                motor::math::vec2f_t p1 = basis * motor::math::vec2f_t(0.0f, +250.0f ) ;
+                // @todo need primitive renderer
+                //_ae.get_prim_render()->draw_line( 0, p0, p1, motor::math::vec4f_t( 0.0f, 1.0f, 0.0f, 1.0f ) ) ;
+            }
+
+            msl_filter_obj.for_each( [&] ( size_t const i, motor::graphics::variable_set_mtr_t vs )
+            {
+                auto * var = vs->data_variable<motor::math::vec3f_t>("plane") ;
+                var->set( dir ) ;
+            } ) ;
+            
+            {
                 motor::graphics::gen4::backend_t::render_detail_t detail ;
+                detail.feed_from_streamout = false ;
+                detail.use_streamout_count = true ;
                 fe->render( &msl_obj, detail ) ;
             }
         }
+        #if 1
+        virtual bool_t on_tool( this_t::window_id_t const, motor::application::app::tool_data_ref_t ) noexcept 
+        {
+            
+            ImGui::Begin( "Test Control" ) ;
 
-        virtual void_t on_shutdown( void_t ) noexcept {}
+            {
+                motor::math::vec2f_t d = dir.xy() ;
+                if( motor::tool::custom_imgui_widgets::direction( "dir", d ) )
+                {
+                    dir = motor::math::vec3f_t( d, 0.0f ) ;
+                }
+            }
+
+            ImGui::End() ;
+            
+            return true ;
+        }
+        #endif
     };
 }
 
