915912f6e9230090ca945a066fcbd381f4ff89a5
aconstlink <aconstlink@gmail.com>
2024-03-17 17:45:15 +0100

    :tada: first test for midi devices

---
+++ suite_device/06_midi_device.cpp
@@ -0,0 +1,69 @@
+
+
+#include <motor/platform/global.h>
+
+#include <motor/math/vector/vector3.hpp>
+#include <motor/math/vector/vector4.hpp>
+#include <motor/math/matrix/matrix4.hpp>
+
+#include <motor/device/system.h>
+#include <motor/device/midi_observer.hpp>
+#include <motor/device/layouts/midi_controller.hpp>
+
+#include <motor/log/global.h>
+
+using namespace motor::core::types ;
+
+namespace this_file
+{
+    class my_observer : public motor::device::midi_observer
+    {
+        virtual void_t on_message( motor::string_cref_t dname, motor::device::midi_message_cref_t msg ) noexcept
+        {
+            motor::device::midi_observer::on_message( dname, msg ) ;
+            
+            
+        }
+    };
+}
+void_t test_device( motor::device::midi_device_mtr_t dev ) 
+{
+
+}
+
+int main( int argc, char ** argv )
+{
+    motor::device::midi_device_ptr_t dev = nullptr ;
+
+    motor::application::carrier_mtr_t carrier = motor::platform::global_t::create_carrier() ;
+
+    this_file::my_observer * obs = motor::memory::create_ptr<this_file::my_observer>() ;
+    carrier->device_system()->install( obs ) ;
+
+    size_t i=0;
+    while( i++ < 5000 )
+    {
+        std::this_thread::sleep_for( std::chrono::milliseconds(20) ) ;
+
+        carrier->device_system()->search( [&] ( motor::device::idevice_mtr_t dev_in )
+        {
+            if( auto * ptr1 = dynamic_cast<motor::device::midi_device_ptr_t>(dev_in); ptr1 != nullptr )
+            {
+                // just take the first one
+                // or test for the name ptr1->name()
+                dev = ptr1 ;
+            }
+        } ) ;
+
+        carrier->device_system()->update() ;
+
+        test_device( dev ) ;
+    }
+
+    motor::memory::release_ptr( obs ) ;
+    motor::memory::release_ptr( carrier ) ;
+    motor::log::global::deinit() ;
+    motor::memory::global_t::dump_to_std() ;
+
+    return 0 ;
+}
