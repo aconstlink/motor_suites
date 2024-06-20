504922727b3454a0d591246e27986568ee16c290
Alex Link <aconstlink@gmail.com>
2024-06-20 09:42:15 +0200

    first test for aseprite format module. not complete

---
+++ suite_format/02_aseprite_json_import.cpp
@@ -0,0 +1,41 @@
+
+
+
+#include <motor/format/global.h>
+#include <motor/format/aseprite/aseprite_module.h>
+
+#include <motor/io/database.h>
+#include <motor/log/global.h>
+
+#include <motor/math/vector/vector4.hpp>
+
+namespace this_file
+{
+    using namespace motor::core::types ;
+
+    // sprite sheet maybe
+}
+
+//
+int main( int argc, char ** argv )
+{
+    motor::format::module_registry_mtr_t mod_reg = motor::format::global::register_default_modules( 
+        motor::shared( motor::format::module_registry_t(), "mod registry"  ) ) ;
+    
+    motor::io::database_t db = motor::io::database_t( motor::io::path_t( DATAPATH ), "./working", "data" ) ;
+
+    // import
+    {
+        auto item = mod_reg->import_from( motor::io::location_t( "aseprite.a_stick_figure.json" ), "aseprite", &db ) ;
+    }
+
+    
+
+    motor::memory::release_ptr( mod_reg ) ;
+
+    motor::io::global_t::deinit() ;
+    motor::log::global_t::deinit() ;
+    motor::memory::global_t::dump_to_std() ;
+
+    return 0 ;
+}
